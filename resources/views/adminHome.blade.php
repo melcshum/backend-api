b18762d827c68a7b01521669fa4c9709d7e2d4ea
lcshum <melcshum@gmail.com>
2020-08-08 16:11:58 +0800

    setup adminlite with Jeroennoten/Laravel-AdminLTE

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -1,21 +1,34 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        You are Admin.
-                    </div>
+    <div class="card">
+        <div class="card-header">Dashboard</div>
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+            You are Admin.
+            Admin Welcome to this beautiful admin panel.
         </div>
     </div>
-@endsection
+@stop
+
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+
+    </script>
+@stop
+
